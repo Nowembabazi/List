2f4c443f4f0ae7edc88a0840461dec30de74bd29
nickson <nowembabazin8@gmail.com>
2022-10-14 11:35:57 -0500

    edit linters

--- src/index.js
+++ src/index.js
@@ -1,55 +1,54 @@
-import _ from 'lodash';
-import './style.css';
+import _ from "lodash";
+import "./style.css";
 
-import addedTask from "./modules/class.js"
-import { addToLocalStorage, getFromLocalStorage } from "./modules/localstorage.js"
-import displayList from "./modules/displaylist.js"
-import checkBox from "./modules/checkbox.js"
+import addedTask from "./modules/class.js";
+import {
+  addToLocalStorage,
+  getFromLocalStorage,
+} from "./modules/localstorage.js";
+import displayList from "./modules/displaylist.js";
+import checkBox from "./modules/checkbox.js";
 
-
-
-const filledTask = document.getElementById('addButton')
-filledTask.addEventListener('click', () => {
-    const myTask = document.getElementById('addTask')
-    if (myTask.value) {
-        addedTask.taskObject(myTask.value)
-    addedTask.reAssignIndex()
-    displayList()
-    myTask.value = '';
-    checkBox()
-    } else {
-        alert('Kindly fill the task to add')
-    }
-})
+const filledTask = document.getElementById("addButton");
+filledTask.addEventListener("click", () => {
+  const myTask = document.getElementById("addTask");
+  if (myTask.value) {
+    addedTask.taskObject(myTask.value);
+    addedTask.reAssignIndex();
+    displayList();
+    myTask.value = "";
+    checkBox();
+  } else {
+    alert("Kindly fill the task to add");
+  }
+});
 
 const handleInputChange = (id) => {
-   const theid = document.getElementById(`edible${id}`);
-   for(let p of addedTask.taskArray){
-    if(p.index === id){
-        p.description = theid.value
+  const theid = document.getElementById(`edible${id}`);
+  for (let p of addedTask.taskArray) {
+    if (p.index === id) {
+      p.description = theid.value;
     }
-   }
-   addToLocalStorage()
-   displayList()
-}
+  }
+  addToLocalStorage();
+  displayList();
+};
 
-
-
-if (localStorage.getItem('storedTask') == null) {
-    addToLocalStorage();
+if (localStorage.getItem("storedTask") == null) {
+  addToLocalStorage();
 } else {
-    getFromLocalStorage();
+  getFromLocalStorage();
 }
 
-const clearBtn = document.getElementById('clear')
-clearBtn.addEventListener('click', () => {
-    let task = addedTask.getTasks()
-    let newArr = task.filter((each) => each.completed === false)
-    addedTask.taskArray = newArr
-    addToLocalStorage()
-    addedTask.reAssignIndex()
-    window.location.reload()
-})
+const clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", () => {
+  let task = addedTask.getTasks();
+  let newArr = task.filter((each) => each.completed === false);
+  addedTask.taskArray = newArr;
+  addToLocalStorage();
+  addedTask.reAssignIndex();
+  window.location.reload();
+});
 
 window.handleInputChange = handleInputChange;
 checkBox();
